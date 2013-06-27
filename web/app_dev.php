d31e286f15e21daabc999701df0936d5c1030a4a
Pascal de Vink <pascal.de.vink@gmail.com>
2013-06-27 23:22:54 +0200

    Added debugging ip

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 var_dump($_SERVER['REMOTE_ADDR']);
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '77.248.0.11'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '77.248.0.11', '10.240.29.198'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
