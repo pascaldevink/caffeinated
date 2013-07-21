640e15cbca9acf8c87e143d5126baca5f965c32f
Pascal de Vink <pascal.de.vink@gmail.com>
2013-07-21 21:51:01 +0200

    Added LocationController test

---
+++ src/RustigWerken/ApiBundle/Tests/Controller/LocationControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RustigWerken\ApiBundle\Tests\Controller;
+
+class LocationControllerTest extends \PHPUnit_Framework_Testcase
+{
+    public function testGetLocationsAction()
+    {
+        $this->assertTrue(true);
+    }
+}
